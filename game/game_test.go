3ebcc2d8a26d51681ca897e9dcf2b97f790e0853
Adrien GOGOIS <ad.gogois@gmail.com>
2023-09-14 14:46:08 +0200

    Add some tests

--- game/game_test.go
+++ game/game_test.go
@@ -2,9 +2,12 @@ package game
 
 import "testing"
 
+var word = []string{"t", "e", "s", "t"}
+var foundLetters = []string{"t", "e", "_", "t"}
+var goodGuess = "t"
+var badGuess = "m"
+
 func TestLetterInWord(t *testing.T) {
-	word := []string{"t", "e", "s", "t"}
-	goodGuess := "t"
 	hasLetter := letterInWord(goodGuess, word)
 	if !hasLetter {
 		t.Errorf("Word %s contains letter %s, got = %v", word, goodGuess, hasLetter)
@@ -12,10 +15,20 @@ func TestLetterInWord(t *testing.T) {
 }
 
 func TestLetterNotInWord(t *testing.T) {
-	word := []string{"t", "e", "s", "t"}
-	badGuess := "a"
 	hasLetter := letterInWord(badGuess, word)
 	if letterInWord(badGuess, word) {
 		t.Errorf("Word %s does not contain letter %s, got = %v", word, badGuess, hasLetter)
+	}
+}
+
+func TestHasWon(t *testing.T) {
+	if !hasWon(word, word) {
+		t.Errorf("Player should win here. Word = %s, foundLetters = %s", word, word)
+	}
+}
+
+func TestHasNotWonYet(t *testing.T) {
+	if hasWon(word, foundLetters) {
+		t.Errorf("Player should not win here. Word = %s, foundLetters = %s", word, foundLetters)
 	}
 }
